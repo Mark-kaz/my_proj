fe20ed19fe47a431f48b4cfcfcabdba95677da08
Mark Antimonov <mark.antimonov@gmail.com>
2024-12-19 16:34:16 +0300

    LR4

--- services/ml_service/main.py
+++ services/ml_service/main.py
@@ -1,9 +1,21 @@
 import random
 from fastapi import FastAPI
 from api_handler import FastAPIHandler
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Histogram, Gauge, Counter, Summary
 
 app = FastAPI()
 app.handler = FastAPIHandler()
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+
+prediction_metric = Histogram(
+    'prediction_metric_histogram',
+    'histogram of predicted prices',
+    buckets=(1, 5, 10, 15, 20, 25, 30)
+)
+
 
 @app.get('/')
 def root_dir():
@@ -12,6 +24,7 @@ def root_dir():
 @app.post('/api/prediction')
 def make_prediction(flat_id: int, item_features: dict):
     prediction = app.handler.predict(item_features)
+    prediction_metric.observe(prediction)
     return ({
              'price': prediction,
              'flat_id': flat_id
